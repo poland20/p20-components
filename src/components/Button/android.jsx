29a320757d54e1d85244a6f83b577693defa85f8
Artur96 <artur@komoter.pl>
2018-06-25 19:58:43 +0100

    Added initial Android button component

--- src/components/Button/android.jsx
+++ src/components/Button/android.jsx
@@ -1,1 +1,15 @@
-// TODO
+// @flow
+
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'native-base';
+
+const style = StyleSheet.create({
+  button: {
+    backgroundColor: '#E2445C',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+});
+
+export default (props: any) => <Button {...props} style={[style.button, props.style]} />;
--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -4,9 +4,17 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { Text } from 'native-base';
 import { Button, NavButton } from '../components/Button/web';
+
+import AndroidButton from '../components/Button/android';
 
 storiesOf('Button', module)
   .add('default view', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('default view (Android)', () => (
+    <AndroidButton>
+      <Text>Hello Android</Text>
+    </AndroidButton>
+  ))
   .add('hollow', () => <Button hollow onClick={action('clicked')}>Hollow Button</Button>)
   .add('NavButton', () => <NavButton href="https://google.com" target="_blank">Go to Google</NavButton>);
