c4f7b53cd80c19b747c1895185a0a7229b464bd5
Artur96 <artur@komoter.pl>
2018-08-09 17:49:58 +0100

    Lazy Image component in progress...

--- src/components/LazyImage/web.tsx
+++ src/components/LazyImage/web.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
+import 'intersection-observer';
+import Observer from '@researchgate/react-intersection-observer';
 
-// const style = css({
-//   position: 'relative',
-//   overflow: 'hidden',
-// });
+import Spinner from '../Spinner/web';
 
-interface Props {
-  source: string;
-  placeholder?: string;
-  aspectRatio: number;
-  method: 'scroll' | 'immediate';
-}
-
-interface SpacerProps {
-  ratio: number;
-}
-
-const Spacer = styled('div')((props: SpacerProps) => ({
+const Spacer = styled('div')((props: { ratio: number }) => ({
   paddingBottom: `${Math.ceil(1 / props.ratio * 100)}%`,
 }));
 
@@ -42,15 +30,56 @@ const Preview = styled('img')(
   },
 );
 
+interface Props {
+  source: string;
+  placeholder?: string;
+  aspectRatio: number;
+  method: 'scroll' | 'immediate';
+}
+
+interface State {
+  image?: HTMLImageElement;
+  // isIntersecting: boolean;
+}
+
 export default class LazyImage extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  state: State = {
+    image: undefined,
+  };
+
+  fetch = (href: string) {
+
+  }
+
+  handleIntersection = ({ isIntersecting }: { isIntersecting: boolean }) => {
+    if (isIntersecting) {
+      this.fetch(this.props.source);
+    }
+  }
+
   render() {
+    const options = {
+      onChange: this.handleIntersection,
+      root: '.site-container',
+    };
+
     return (
-      <React.Fragment>
-        <Spacer ratio={this.props.aspectRatio}/>
-        {this.props.placeholder ?
-        <Preview src={this.props.placeholder} role="presentation" aria-hidden={true}/> : null
+      <Observer {...options}>
+        { this.state.image ?
+        <React.Fragment>
+          <Spacer ratio={this.props.aspectRatio}/>
+          {this.props.placeholder ?
+          <Preview src={this.props.placeholder} role="presentation" aria-hidden={true}/> : null
+          }
+          <Spinner/>
+        </React.Fragment>
+        : <React.Fragment/>
         }
-      </React.Fragment>
+      </Observer>
     );
   }
 }
