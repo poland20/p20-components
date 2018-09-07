cc9fba0a705616509acb29e2892a07161830f1e3
Artur96 <artur@komoter.pl>
2018-09-07 19:51:45 +0100

    TypeScript definition files

---
+++ src/components/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'typography';
+declare module 'react-typography';
+declare module 'react-id-swiper';
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,20 @@
+import * as React from 'react';
+
 declare module 'typography';
 declare module 'react-typography';
 declare module 'react-id-swiper';
-declare module "*.svg" {
-  const content: string;
-  export default content;
-}
+
+export { default as Agenda } from './components/Agenda/web';
+export { default as AnimatedLogo } from './components/AnimatedLogo/web';
+export { default as Banner } from './components/Banner/web';
+export { default as Button, NavButton } from './components/Button/web';
+export { default as Card, CardList } from './components/Card/web';
+export { default as Container } from './components/Container/web';
+export { default as Countdown } from './components/Countdown/web';
+export { default as LazyImage } from './components/LazyImage/web';
+export { default as Loader } from './components/Loader/web';
+export { default as Modal } from './components/Modal/web';
+export { default as ResponsiveVideo } from './components/ResponsiveVideo/web';
+export { default as Spinner } from './components/Spinner/web';
+export { default as TopNavigation } from './components/TopNavigation/web';
+export { default as typography } from './components/typography';
